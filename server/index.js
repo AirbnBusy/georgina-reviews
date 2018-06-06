06619526f35bdba4d8ac1ad1b84123063bc9eb6c
Georgina Lopez <georginalopez@georginas-mbp.lan>
2018-06-05 22:52:15 -0700

    added test for App component

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
+const app = express();
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
